cd6c6e06ba6038206d7322ef925d48ed731b4dee
mjsn98 <mjsn98@gmail.com>
2023-10-25 21:04:58 -0300

    add estilos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.listen(3000,()=>console.log("Exito")); 
