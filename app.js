cfdca4957b261c9b9f462061a54186b0d2ba9620
mjsn98 <mjsn98@gmail.com>
2023-11-01 20:36:31 -0300

    diseños

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ app.listen(3000,()=>console.log("Exito"));
 app.get('/',function(req,res){ 
     res.sendFile(path.join(__dirname,'views/home.html')); 
 });
+app.get('/prueba',function(req,res){ 
+    res.sendFile(path.join(__dirname,'views/prueba.html')); 
+});
