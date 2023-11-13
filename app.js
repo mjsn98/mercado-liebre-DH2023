f96eb00d965938f4fa74b0c757b10de31e7a9891
mjsn98 <mjsn98@gmail.com>
2023-11-13 20:10:48 -0300

    configuracion para deploy

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const port=process.env.PORT || 3001;
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.listen(3001,()=>console.log("Exito")); 
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`)); 
 app.get('/',function(req,res){ 
     res.sendFile(path.join(__dirname,'views/home.html')); 
 });
+
 app.get('/login',function(req,res){ 
     res.sendFile(path.join(__dirname,'views/login.html')); 
 });
