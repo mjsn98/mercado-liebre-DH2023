87159f63894a03ad6bf9d45d1c051ceaa208573a
mjsn98 <mjsn98@gmail.com>
2023-11-13 14:24:04 -0300

    actualizacion

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path=require('path');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.listen(3000,()=>console.log("Exito")); 
+app.listen(3001,()=>console.log("Exito")); 
 app.get('/',function(req,res){ 
     res.sendFile(path.join(__dirname,'views/home.html')); 
 });
